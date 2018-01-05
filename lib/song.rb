a7892d83ec843dd2664d67c7e9d7bd7fabc92d9f
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-05 00:24:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,8 @@ class Song
 
   def self.new_by_filename(filename)
     song = Song.new(filename.split(' - ')[1])
-  #  binding.pry
     song.artist = Artist.find_or_create_by_name(filename.split(' - ')[0])
     song
-
   end
 
 
